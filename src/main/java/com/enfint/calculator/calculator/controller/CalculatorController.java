fdf29ce15366b1497db7f878ee40d51c7da895aa
Ngcobo <khayelihle.ngcobo@siemens.com>
2022-08-17 20:27:12 +0200

    Divide by 0 exception handled

--- src/main/java/com/enfint/calculator/calculator/controller/CalculatorController.java
+++ src/main/java/com/enfint/calculator/calculator/controller/CalculatorController.java
@@ -1,9 +1,11 @@
 package com.enfint.calculator.calculator.controller;
 
 import com.enfint.calculator.calculator.model.Calculator;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class CalculatorController {
@@ -25,6 +27,11 @@ public class CalculatorController {
 
     @GetMapping("/divide/{first_number}/{second_number}")
     public double divide(@PathVariable double first_number, @PathVariable double second_number){
-        return new Calculator(first_number, second_number, first_number/second_number).getResult();
+        try{
+            return new Calculator(first_number, second_number, first_number/second_number).getResult();
+        }catch (ArithmeticException e){ //cannot divide by zero
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot divide by zero!",e);
+        }
+
     }
 }
